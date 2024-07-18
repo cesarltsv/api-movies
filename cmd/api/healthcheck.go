6ef1bb1cb03f45c5de5eee7a44b3b7a8f7ad51c3
cezarltsv <cezarltsv@gmail.com>
2024-07-17 23:26:06 -0300

    create server

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,7 @@
+package main
+
+// func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "status available")
+// 	fmt.Fprintf(w, "environment %s\n", app.config.env)
+// 	fmt.Fprintf(w, "version %s\n", version)
+// }
